b834cc35bab6aa664db4518a98f2f75711da738a
Slenky <slenky@protonmail.com>
2021-06-30 11:49:30 +0200

    ⏰ Added some more advanced stuff

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ app.use(express.static('public'))
 
 
 app.get('/', function (req,res ) {
-    res.sendFile(__dirname + '/cat.jpg');
+    res.sendFile(__dirname + '/sites/' + 'landing.html');
 })
 
 app.get('/*', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET");
-    res.status(404).sendFile(__dirname + '/rip.jpg');
+    res.status(404).sendFile(__dirname + '/sites/' + '404.html');
+    res.status(401).sendFile(__dirname + '/sites/' + 'landing.html');
+    res.status(403).sendFile(__dirname + '/sites/' + 'landing.html');
 })
 
 app.listen('80')
