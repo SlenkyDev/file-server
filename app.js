1ef80d1138efaa6ab3756e56e4e371f162f51b90
Slenky <slenky@protonmail.com>
2021-07-15 18:24:49 +0200

    🙄Removed some useless bits of code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express'),
 app = express(),
-path = require('path');
 
 app.use(express.static('public'))
 
@@ -14,8 +13,6 @@ app.get('/*', function (req, res) {
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET");
     res.status(404).sendFile(__dirname + '/sites/' + '404.html');
-    res.status(401).sendFile(__dirname + '/sites/' + 'landing.html');
-    res.status(403).sendFile(__dirname + '/sites/' + 'landing.html');
 })
 
 app.listen('80')
